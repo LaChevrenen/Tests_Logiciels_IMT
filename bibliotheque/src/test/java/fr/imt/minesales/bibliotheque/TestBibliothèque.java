6c318ce558b86094382a60c6ea96ebbdd188d631
Romain Carlino <romain.carlino@etu.mines-ales.fr>
2026-01-27 14:58:02 +0100

    Base classes

---
+++ bibliotheque/src/test/java/fr/imt/minesales/bibliotheque/TestBibliothèque.java
@@ -0,0 +1,14 @@
+package fr.imt.minesales.bibliotheque;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestBibliothèque {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
