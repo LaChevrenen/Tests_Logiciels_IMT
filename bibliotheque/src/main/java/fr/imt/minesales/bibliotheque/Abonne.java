79c1cd6585064ecce51a51396c5b2b9b69b1b0fc
Romain Carlino <romain.carlino@etu.mines-ales.fr>
2026-02-04 10:04:19 +0100

    Implemented Abonne.java methods

--- bibliotheque/src/main/java/fr/imt/minesales/bibliotheque/Abonne.java
+++ bibliotheque/src/main/java/fr/imt/minesales/bibliotheque/Abonne.java
@@ -2,23 +2,23 @@ package fr.imt.minesales.bibliotheque;
 
 /**
  * Classe Abonné - TDD
- * Signatures minimales pour compilation
- * Implémentation mockée dans les tests
  */
 public class Abonne {
 	
-	// Méthodes nécessaires pour la compilation des tests
-	// L'implémentation réelle sera ajoutée plus tard
+	private String nom;
+	private String prenom;
+	private String numeroAbonne;
+	
 	
 	public String getNom() {
-		return null; // À implémenter
+		return this.nom;
 	}
 
 	public String getPrenom() {
-		return null; // À implémenter
+		return this.prenom;
 	}
 
 	public String getNumeroAbonne() {
-		return null; // À implémenter
+		return this.numeroAbonne;
 	}
 }
